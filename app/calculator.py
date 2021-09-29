03be4e9c0d1623c50fcb849ae533245cb11417c8
Jonathan Yip <jyip0005@student.monash.edu>
2021-09-29 16:52:31 +0800

    test

--- app/calculator.py
+++ app/calculator.py
@@ -1,13 +1,40 @@
 from datetime import date, datetime
 from workalendar.oceania import Australia
 
+
+class Charger_Configurations:
+    configuration = {"1": {"power": 2,
+                           "base": 5},
+                     "2": {"power": 3.6,
+                           "base": 7.5},
+                     "3": {"power": 7.2,
+                           "base": 10},
+                     "4": {"power": 11,
+                           "base": 12.5},
+                     "5": {"power": 22,
+                           "base": 15},
+                     "6": {"power": 36,
+                           "base": 20},
+                     "7": {"power": 90,
+                           "base": 30},
+                     "8": {"power": 350,
+                           "base": 50},
+                     }
+
+    def get_power(self, config):
+        return self.configuration[str(config)]["power"]
+
+    def get_base_price(self, base_price):
+        return self.configuration[str(base_price)]["base"]
+
+
 class Calculator():
-    # you can choose to initialise variables here, if needed.s
+    # you can choose to initialise variables here, if needed.
     def __init__(self):
         pass
 
     # you may add more parameters if needed, you may modify the formula also.
-    def cost_calculation(self, initial_state, final_state, capacity, is_peak, is_holiday):
+    def cost_calculation(self, configuration, initial_state, final_state, capacity, is_peak, is_holiday):
         if is_peak:
             base_price = 100
         else:
@@ -17,44 +44,49 @@ class Calculator():
             surcharge_factor = 1.1
         else:
             surcharge_factor = 1
-
-        cost = (final_state - initial_state) / 100 * capacity * base_price / 100 * surcharge_factor
+        bp = Charger_Configurations().get_base_price(configuration)
+        cost = (final_state - initial_state) / 100 * capacity * base_price * bp / 100 * surcharge_factor
         return cost
 
     # you may add more parameters if needed, you may also modify the formula.
-    def time_calculation(self, initial_state, final_state, capacity, power):
-        time = (final_state - initial_state) / 100 * capacity / power
-        return time
+    def time_calculation(self, configuration, initial_state, final_state, capacity):
+        # power=get_power(configuration)
+        power = Charger_Configurations.get_power(configuration)
+        time = ((final_state - initial_state) / 100 * capacity / power) * 60  # in minutes
+        return round(time)
 
-
-    # you may create some new methods at your convenience, or modify these methods, or choose not to use them.
     def is_holiday(self, start_date):
-        #use the workalender module and weekends
-        #start date is a string
+        # use the workalender module and weekends
+        # start date is a string
         cal = Australia()
         date_str = datetime.strptime(start_date, "%Y-%m-%d")
-        #check if its a holiday or a weekend 
+        # check if its a holiday or a weekend
         return (not cal.is_working_day(date_str)) or date_str.weekday() > 4
 
-    def is_peak(self):
-        pass
+    def is_peak(self, time):
+        # assuming that the time is in 24 hr format
+        ti = datetime.strptime(time, "%H:%M")
+        return ti >= datetime.strptime("06:00", "%H:%M") and ti < datetime.strptime("18:00", "%H:%M")
 
-    def peak_period(self, start_time):
-        pass
+    # def peak_period(self, start_time):
+    #
 
     def get_duration(self, start_time):
+        # total charging time of the car
         pass
 
     # to be acquired through API
     def get_sun_hour(self, sun_hour):
         pass
 
-    # to be acquired through API
+    # to be acquired through API --
     def get_solar_energy_duration(self, start_time):
+        # duration in which car charges while in day light
         pass
 
-    # to be acquired through API
+    # to be acquired through API --
     def get_day_light_length(self, start_time):
+        # sunrise - sunset
         pass
 
     # to be acquired through API
@@ -67,5 +99,10 @@ class Calculator():
 
     def calculate_solar_energy(self):
         pass
+
+
+
+
+
 
 
--- test/pyunittest.py
+++ test/pyunittest.py
@@ -1,25 +1,69 @@
-import os
-from src.utils.FileManager import FileManager
 from app.calculator import *
 import unittest
 
-class TestCalculator(unittest.TestCase):
+from project.app.calculator import Calculator
 
+
+class TestCalculator(unittest.TestCase):
     # you may create more test methods
     # you may add parameters to test methods
     # this is an example
     def test_cost(self):
-            self.calculator = Calculator()
-            test_configuration="1"
-            test_initial_state=10
-            test_final_state=100
-            test_capacity=80
+        self.calculator = Calculator()
+        test_configuration = "1"
+        test_initial_state = 10
+        test_final_state = 100
+        test_capacity = 80
 
-            self.assertEqual(self.calculator.is_holiday("2021-01-01"), True)
-            self.assertEqual(self.calculator.is_holiday("2021-01-02"), False)
-            self.assertEqual(self.calculator.is_holiday("2021-01-09"), True)
+        """
+        """
+        self.assertEqual(self.calculator.is_holiday("2021-01-01"), True)
+        self.assertEqual(self.calculator.is_holiday("2021-01-02"), False)
+        self.assertEqual(self.calculator.is_holiday("2021-01-09"), True)
 
+        self.assertEqual(self.calculator.is_peak("05:59"), False)
+        self.assertEqual(self.calculator.is_peak("06:00"), True)
+        self.assertEqual(self.calculator.is_peak("18:00"), True)
+        self.assertEqual(self.calculator.is_peak("18:01"), False)
 
+        """
+        Pairwise testing was used for 
+        """
+        self.assertEqual(
+            self.calculator.cost_calculation(test_configuration, test_initial_state, test_final_state, test_capacity,
+                                             True, True), "")
+        self.assertEqual(
+            self.calculator.cost_calculation(test_configuration, test_initial_state, test_final_state, test_capacity,
+                                             False, True), "")
+        self.assertEqual(
+            self.calculator.cost_calculation(test_configuration, test_initial_state, test_final_state, test_capacity,
+                                             True, False), "")
+        self.assertEqual(
+            self.calculator.cost_calculation(test_configuration, test_initial_state, test_final_state, test_capacity,
+                                             False, False), "")
+
+    def test_time(self):
+        self.calculator = Calculator()
+        test_configuration = "1"
+        test_initial_state = 10
+        test_final_state = 100
+        test_capacity = 80
+        self.assertEqual(self.calculator.time_calculation("2", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("1", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("3", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("4", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("5", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("6", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("7", test_initial_state, test_final_state, test_capacity),
+                         False)
+        self.assertEqual(self.calculator.time_calculation("8", test_initial_state, test_final_state, test_capacity),
+                         False)
 
     # you may create test suite if needed
     if __name__ == "__main__":
