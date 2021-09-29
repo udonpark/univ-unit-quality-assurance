a930a763634efa76e75c95d54deea5e3950cf95f
smal0037 <smal0037@student.monash.edu>
2021-09-29 16:54:56 +0400

    Pushing implementation of solar energy calculation

--- app/calculator.py
+++ app/calculator.py
@@ -43,16 +43,111 @@ class Calculator():
         pass
 
     # to be acquired through API
-    def get_sun_hour(self, sun_hour):
-        pass
+    def get_sun_hour(self, postcode, start_date):
+        location_id = ""
+        start_date1 = str(start_date)[::-1] #start date in reverse form as start date input is in reverse in the api
+
+        #for the location id
+        response = requests.get('http://118.138.246.158/api/v1/location?postcode='+str(postcode))
+        response_data = response.json()
+
+        for a in response_data:
+            location_id += a['id']
+
+        # for sun hours duration
+        response1 = requests.get('http://118.138.246.158/api/v1/weather?location=' + location_id + '&date=' + start_date1)
+        response_data1 = response1.json()
+        return response_data1['sunHours'] #sunHours aka solar insolation
 
     # to be acquired through API
-    def get_solar_energy_duration(self, start_time):
-        pass
+    def get_day_light_length(self, postcode, start_date, start_time):
+        location_id = ""
+        start_date1 = str(start_date)[::-1] #start date in reverse form as start date input is in reverse in the api
 
-    # to be acquired through API
-    def get_day_light_length(self, start_time):
-        pass
+        #for the location id
+        response = requests.get('http://118.138.246.158/api/v1/location?postcode='+str(postcode))
+        response_data = response.json()
+
+        for a in response_data:
+            location_id += a['id']
+
+        # for sun hours duration
+        response1 = requests.get('http://118.138.246.158/api/v1/weather?location=' + location_id + '&date=+' + start_date1)
+        response_data1 = response1.json()
+
+        sunrise = response_data1['sunrise']
+        sunrise = sunrise.split(":")
+        sunrisehours = sunrise[0]
+        sunrisemins = sunrise[1]
+
+        sunset = response_data1['sunset']
+        sunset = sunset.split(":")
+        sunsethours = sunset[0]
+        sunsetmins = sunset[1]
+
+        hours_difference = sunsethours - sunrisehours
+        mins_difference = sunsetmins - sunrisemins
+
+        return hours_difference + (mins_difference/60)
+
+    def add_time(self, time1, time2):
+        # add two times in string format. e.g., "10:30" + "4:40" -> "15:10"
+        time1 = time1.split(":")
+        time2 = time2.split(":")
+        sum_mins = int(time1[0]) * 60 + int(time2[0]) * 60 + int(time1[1]) + int(time2[1])
+        sum_hours = str(sum_mins // 60) + ":" + str(sum_mins % 60)
+        return sum_hours
+
+    def minus_time(self, start_time, end_time):
+        # minus two times in string format. e.g., "10:30" - "4:40" -> "5:50"
+        start_time = start_time.split(":")
+        end_time = end_time.split(":")
+        difference_mins = (int(end_time[0]) * 60 + int(end_time[1])) - (int(start_time[0]) * 60 + int(start_time[1]))
+        difference_hours = str(difference_mins // 60) + ":" + str(difference_mins % 60)
+        return difference_hours
+
+    def mins_to_hours(self, mins):
+        # converts minutes to time format. e.g., "150" -> 2:30
+        return str(mins // 60) + ":" + str(mins % 60)
+
+    def hours_to_mins(self, hours):
+        # converts hours to minutes. e.g., "2:30" -> 150
+        hours = hours.split(":")
+        return int(hours[0]) * 60 + int(hours[1])
+
+    # to be acquired through API --
+    def get_solar_energy_duration(self, postcode, start_date, start_time, charging_length):
+        # duration in which car charges while in day light
+        location_id = ""
+        start_date1 = str(start_date)[::-1]  # start date in reverse form as start date input is in reverse in the api
+
+        # for the location id
+        response = requests.get('http://118.138.246.158/api/v1/location?postcode=' + str(postcode))
+        response_data = response.json()
+
+        for a in response_data:
+            location_id += a['id']
+
+        # for sun hours duration
+        response1 = requests.get(
+            'http://118.138.246.158/api/v1/weather?location=' + location_id + '&date=+' + start_date1)
+        response_data1 = response1.json()
+
+        # obtain sunrise and sunset times, and also end time of the charging process
+        end_time = self.add_time(start_time, self.minus_time(charging_length))
+        sunrise = response_data1['sunrise']
+        sunset = response_data1['sunset']
+
+        # make sure to calculate charging time during daylight hours
+        if self.hours_to_mins(sunrise) > self.hours_to_mins(start_time):
+            start_time = sunrise
+
+        if self.hours_to_mins(sunset) < self.hours_to_mins(end_time):
+            end_time = sunset
+
+        # calculate and return duration time in minutes
+        duration_hours = self.minus_time(start_time, end_time)
+        return self.hours_to_mins(duration_hours)
 
     # to be acquired through API
     def get_cloud_cover(self, postcode, start_date, start_time):
