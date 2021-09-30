bec46c496ccbc9baa66c1f08a35ff6756915d0c9
yoo <ykog0002@student.monash.edu>
2021-10-01 01:37:59 +0900

    improved solar_energy_aux

--- test/pyunittest.py
+++ test/pyunittest.py
@@ -103,9 +103,10 @@ class TestCalculator(unittest.TestCase):
         #self.assertEqual(cal.solar_energy_aux("01-01-2020", "21:00", "3800", 80, 20, 80, 350),([], [], '19:45') )
         self.assertEqual(cal.solar_energy_aux("01-01-2020", "12:00", "3800", 80, 20, 80, 350),([0.05966101694915255], ['12:00', '13:00'], '13:30') )
         self.assertEqual(cal.solar_energy_aux("01-01-2020", "23:00", "3800", 80, 20, 80, 350),([], [], '19:45'))
-   
-   
-   #MOCKING for api functions
+
+    def calculate_solar_energy(self):
+        pass
+    # MOCKING for api functions
     def test_get_sun_hour(self):
         pass
 
