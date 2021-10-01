6929531280af8bf136749ffcbddecad152ebf92b
Jonathan Yip <jyip0005@student.monash.edu>
2021-10-01 15:27:12 +0800

    update mocking

--- test/mocktesting.py
+++ test/mocktesting.py
@@ -12,14 +12,14 @@ from app.calculator import *
 
 import unittest
 from unittest.mock import Mock
-
+def test_mock():
    mock = Mock()
         mock.return_value = {
             "postcode": "3800",
             "date":"01-01-2020",
         }
 
-     Calculator.is_holiday = Mock()
+    Calculator.is_holiday = Mock()
         holi_new_yr = "2020-01-01"
         Calculator.return_value = True
         assert Calculator.is_holiday()
